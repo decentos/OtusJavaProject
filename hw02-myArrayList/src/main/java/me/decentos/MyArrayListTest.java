d30e194cc745e4e3dcd89c401b995926a1d23dc9
Vlad Matveev <matveev_vlad@me.com>
2020-01-20 20:58:28 +0300

    Добавлена зависимость Maven в модуль hw02-myArrayList

--- hw02-myArrayList/src/main/java/me/decentos/MyArrayListTest.java
+++ hw02-myArrayList/src/main/java/me/decentos/MyArrayListTest.java
@@ -3,7 +3,6 @@ package me.decentos;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.ArrayList;
 
 public class MyArrayListTest {
     public static void main(String[] args) {
