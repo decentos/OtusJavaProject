4a6f01c1189dd93a60fb6d71f957e4a774c158be
Vlad Matveev <matveev_vlad@me.com>
2020-02-05 19:20:08 +0300

    Выполнено домашнее задание №4

--- hw04-autoLogging/src/main/java/me/decentos/Demo.java
+++ hw04-autoLogging/src/main/java/me/decentos/Demo.java
@@ -2,10 +2,14 @@ package me.decentos;
 
 public class Demo {
     public static void main(String[] args) {
-        new Demo().action();
+        try {
+            new Demo().action();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void action() {
+    public void action() throws NoSuchMethodException {
         Logging logging = ProxyIoC.createMyClass();
         logging.calculation(6);
     }
--- hw04-autoLogging/src/main/java/me/decentos/ProxyIoC.java
+++ hw04-autoLogging/src/main/java/me/decentos/ProxyIoC.java
@@ -3,11 +3,10 @@ package me.decentos;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
 
 public class ProxyIoC {
 
-    static Logging createMyClass() {
+    static Logging createMyClass() throws NoSuchMethodException {
         InvocationHandler handler = new LoggingInvocationHandler(new LoggingImpl());
         return (Logging) Proxy.newProxyInstance(ProxyIoC.class.getClassLoader(),
                 new Class<?>[]{Logging.class}, handler);
@@ -15,17 +14,20 @@ public class ProxyIoC {
 
     static class LoggingInvocationHandler implements InvocationHandler {
         private final Logging logging;
+        private final Class<LoggingImpl> annotatedClass = LoggingImpl.class;
+        private final Method calculationMethod = annotatedClass.getMethod("calculation", int.class);
+        private final Log logAnnotationBeforeMethod = calculationMethod.getDeclaredAnnotation(Log.class);
 
-        LoggingInvocationHandler(Logging logging) {
+        LoggingInvocationHandler(Logging logging) throws NoSuchMethodException {
             this.logging = logging;
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-
-            System.out.println("executed method: " + method.getName() + ", param: " + Arrays.toString(args));
+            if (logAnnotationBeforeMethod != null) {
+                System.out.println("executed method: " + method.getName() + ", param: " + args[0]);
+            }
             return method.invoke(logging, args);
         }
     }
 }
-
