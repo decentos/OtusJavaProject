2a9e41e8f64cf7a80f03240f43ccab1e953a207e
Vlad Matveev <49278415+decentos@users.noreply.github.com>
2020-02-06 16:10:21 +0300

    Исправлены замечания по ДЗ №4

--- hw04-autoLogging/src/main/java/me/decentos/ProxyIoC.java
+++ hw04-autoLogging/src/main/java/me/decentos/ProxyIoC.java
@@ -3,30 +3,32 @@ package me.decentos;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
 
 public class ProxyIoC {
 
-    static Logging createMyClass() throws NoSuchMethodException {
+    static Logging createMyClass() {
         InvocationHandler handler = new LoggingInvocationHandler(new LoggingImpl());
-        return (Logging) Proxy.newProxyInstance(ProxyIoC.class.getClassLoader(),
-                new Class<?>[]{Logging.class}, handler);
+        return (Logging) Proxy.newProxyInstance(ProxyIoC.class.getClassLoader(), new Class<?>[]{Logging.class}, handler);
     }
 
     static class LoggingInvocationHandler implements InvocationHandler {
         private final Logging logging;
-        private final Class<LoggingImpl> annotatedClass = LoggingImpl.class;
-        private final Method calculationMethod = annotatedClass.getMethod("calculation", int.class);
-        private final Log logAnnotationBeforeMethod = calculationMethod.getDeclaredAnnotation(Log.class);
 
-        LoggingInvocationHandler(Logging logging) throws NoSuchMethodException {
+        LoggingInvocationHandler(Logging logging) {
             this.logging = logging;
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Class<?> annotatedClass = this.logging.getClass();
+            Method calculationMethod = annotatedClass.getMethod(method.getName(), method.getParameterTypes());
+            Log logAnnotationBeforeMethod = calculationMethod.getAnnotation(Log.class);
+
             if (logAnnotationBeforeMethod != null) {
-                System.out.println("executed method: " + method.getName() + ", param: " + args[0]);
+                System.out.println("executed method: " + method.getName() + ", param: " + Arrays.toString(args));
             }
+
             return method.invoke(logging, args);
         }
     }
