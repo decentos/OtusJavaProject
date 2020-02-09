bbdbb432559eeb1311cf6499a382217e9e2a30dc
Vlad Matveev <matveev_vlad@me.com>
2020-02-09 10:58:03 +0300

    Выполнено домашнее задание №5

---
+++ hw05-myTestFramework/src/main/java/me/decentos/TestStart.java
@@ -0,0 +1,8 @@
+package me.decentos;
+
+public class TestStart {
+    public static void main(String[] args) {
+        TestMain test = new TestMain("me.decentos.ClassForTest");
+        test.run();
+    }
+}
