74e68fd1f975e575601019b05b2d2a7438fc79e8
Vlad Matveev <matveev_vlad@me.com>
2020-03-28 09:26:46 +0300

    Перенос приложения из hw10-hibernate

---
+++ hw11-myCache/src/main/java/me/decentos/core/service/DbServiceUserImpl.java
@@ -0,0 +1,56 @@
+package me.decentos.core.service;
+
+import me.decentos.core.dao.UserDao;
+import me.decentos.core.model.User;
+import me.decentos.core.sessionmanager.SessionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+public class DbServiceUserImpl implements DBServiceUser {
+    private static Logger logger = LoggerFactory.getLogger(DbServiceUserImpl.class);
+
+    private final UserDao userDao;
+
+    public DbServiceUserImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public long saveUser(User user) {
+        try (SessionManager sessionManager = userDao.getSessionManager()) {
+            sessionManager.beginSession();
+            try {
+                long userId = userDao.saveUser(user);
+                sessionManager.commitSession();
+
+                logger.info("saved user: {}", userId);
+                return userId;
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+                sessionManager.rollbackSession();
+                throw new DbServiceException(e);
+            }
+        }
+    }
+
+
+    @Override
+    public Optional<User> getUser(long id) {
+        try (SessionManager sessionManager = userDao.getSessionManager()) {
+            sessionManager.beginSession();
+            try {
+                Optional<User> userOptional = userDao.findById(id);
+
+                logger.info("loaded user: {}", userOptional.orElse(null));
+                return userOptional;
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+                sessionManager.rollbackSession();
+            }
+            return Optional.empty();
+        }
+    }
+
+}
