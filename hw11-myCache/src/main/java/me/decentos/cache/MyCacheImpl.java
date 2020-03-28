2ed615d519d46248298bbd589e56c8e8e07ace17
Vlad Matveev <matveev_vlad@me.com>
2020-03-28 09:32:09 +0300

    Add cache template

---
+++ hw11-myCache/src/main/java/me/decentos/cache/MyCacheImpl.java
@@ -0,0 +1,29 @@
+package me.decentos.cache;
+
+public class MyCacheImpl<K, V> implements MyCache<K, V> {
+
+  @Override
+  public void put(K key, V value) {
+
+  }
+
+  @Override
+  public void remove(K key) {
+
+  }
+
+  @Override
+  public V get(K key) {
+    return null;
+  }
+
+  @Override
+  public void addListener(Listener<K, V> listener) {
+
+  }
+
+  @Override
+  public void removeListener(Listener<K, V> listener) {
+
+  }
+}
