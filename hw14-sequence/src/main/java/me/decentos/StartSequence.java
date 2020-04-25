77a628c5f92f0a10d74500a80b90d52a91f678bc
Vlad Matveev <matveev_vlad@me.com>
2020-04-25 06:55:54 +0300

    Code-refactoring

--- hw14-sequence/src/main/java/me/decentos/StartSequence.java
+++ hw14-sequence/src/main/java/me/decentos/StartSequence.java
@@ -3,64 +3,41 @@ package me.decentos;
 import lombok.SneakyThrows;
 
 public class StartSequence {
-    private int count = 1;
     private static final int LIMIT = 10;
-    private boolean wakeUp = false;
+    private String threadName = "second";
 
     public static void main(String[] args) {
-        StartSequence sequence = new StartSequence();
-        sequence.go();
+        StartSequence seq = new StartSequence();
+        new Thread(() -> seq.print(1, Thread.currentThread().getName()), "first").start();
+        new Thread(() -> seq.print(1, Thread.currentThread().getName()), "second").start();
     }
 
     @SneakyThrows
-    private void go() {
-        Thread thread1 = new Thread(this::startSeq1);
-        Thread thread2 = new Thread(this::startSeq2);
-
-        thread1.start();
-        thread2.start();
-
-        thread1.join();
-        thread2.join();
-    }
-
-    @SneakyThrows
-    private synchronized void startSeq1() {
+    private synchronized void print(int count, String thisName) {
         for (int i = 0; i < LIMIT; i++) {
-            if (!wakeUp) {
-                System.out.println(count);
-                wait();
-                count++;
-            }
-            wakeUp = false;
-            notify();
-        }
-        for (int i = 0; i < LIMIT - 1; i++) {
-            if (!wakeUp) {
-                count--;
-                System.out.println(count - 1);
-                wait();
-            }
-            wakeUp = false;
-            notify();
-        }
-    }
 
-    @SneakyThrows
-    private synchronized void startSeq2() {
-        for (int i = 0; i < LIMIT; i++) {
-            Thread.sleep(1000);
+            while (thisName.equals(threadName)) {
+                this.wait();
+            }
+
             System.out.println(count);
-            wakeUp = true;
-            notify();
-            wait();
+            count++;
+            threadName = thisName;
+            Thread.sleep(1_000);
+            notifyAll();
         }
+
         for (int i = 0; i < LIMIT - 1; i++) {
-            Thread.sleep(1000);
+
+            while (thisName.equals(threadName)) {
+                this.wait();
+            }
+
+            count--;
             System.out.println(count - 1);
-            wakeUp = true;
-            notify();
-            wait();
+            threadName = thisName;
+            Thread.sleep(1_000);
+            notifyAll();
         }
     }
 }
